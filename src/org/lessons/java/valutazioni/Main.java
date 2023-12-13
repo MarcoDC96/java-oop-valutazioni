c14cbfdeffb2af04dcc74210f744af58363958fd
MarcoDC96 <marco.decicco@live.it>
2023-12-13 15:17:00 +0100

    Initial commit, start.

---
+++ src/org/lessons/java/valutazioni/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.valutazioni;
+
+public class CorsoMain {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            
+        }
+    }
+}
